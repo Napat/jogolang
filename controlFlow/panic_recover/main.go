e58a71271ece78608828b2604f7ad3a160bc0d52
Napat Rungruangbangchan <napat.joe@gmail.com>
2016-04-28 18:18:05 +0700

    Panic/Recovery with forward error

--- controlFlow/panic_recover/main.go
+++ controlFlow/panic_recover/main.go
@@ -22,15 +22,35 @@ func fileLine() string {
 	return s
 }
 
-func f() {
+func f() (err error) {
 	defer func() {
 		if r := recover(); r != nil {
-			fmt.Println("Found panic: ", r)
-			fmt.Println("Recovered")
+			fmt.Println("Found panic")
+			if _, ok := r.(runtime.Error); ok {
+				fmt.Println("runtime panic")
+				panic(r)
+			}
+			if s, ok := r.(string); ok {
+				fmt.Println(fileLine(), "found panic: ", s)
+				panic(s)
+			}
+			err = r.(error) // forward error
+
+			//more example about panic
+			////https://golang.org/src/encoding/json/encode.go?s=5584:5627#L127
 		}
 	}()
 	g(0)
 	fmt.Println("End of f()")
+	return nil
+}
+
+type gError struct {
+	count int
+}
+
+func (e *gError) Error() string {
+	return fmt.Sprintf("Count=%v at %v", e.count, fileLine())
 }
 
 func g(count int) {
@@ -38,13 +58,15 @@ func g(count int) {
 	fmt.Printf("Try g(%v)...", count)
 	if count > 1 {
 		fmt.Println("Panicking!")
-		panic(fmt.Sprintf("Count=%v at %v", count, fileLine()))
+		panic(&gError{count})
 	}
 	fmt.Printf("g(%v) is ok\n", count)
 	g(count + 1)
 }
 
 func main() {
-	f()
+	if r := f(); r != nil {
+		fmt.Println(r)
+	}
 	fmt.Println("End main()")
 }
