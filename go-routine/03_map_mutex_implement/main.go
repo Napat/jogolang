2d27312e0f8689b20d0a9b362f4e60af948a17fc
Napat Rungruangbangchan <napat.joe@gmail.com>
2016-05-08 15:00:40 +0700

    map mutex: thread safe implementation for concurrent used

---
+++ go-routine/03_map_mutex_implement/main.go
@@ -0,0 +1,50 @@
+/*
+Maps are not safe for concurrent use:
+It's not defined what happens when you read and write to them simultaneously.
+
+If there is at least one writer and at least one more either writer or reader,
+then all readers and writers must use synchronization to access the map.
+A mutex works fine for this.
+
+https://tour.golang.org/concurrency/9
+*/
+
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// SafeCounter is safe to use concurrently.
+type SafeCounter struct {
+	v   map[string]int
+	mux sync.Mutex
+}
+
+// Inc increments the counter for the given key.
+func (c *SafeCounter) Inc(key string) {
+	c.mux.Lock()
+	// Lock so only one goroutine at a time can access the map c.v.
+	c.v[key]++
+	c.mux.Unlock()
+}
+
+// Value returns the current value of the counter for the given key.
+func (c *SafeCounter) Value(key string) int {
+	c.mux.Lock()
+	// Lock so only one goroutine at a time can access the map c.v.
+	defer c.mux.Unlock()
+	return c.v[key]
+}
+
+func main() {
+	c := SafeCounter{v: make(map[string]int)}
+	for i := 0; i < 1000; i++ {
+		go c.Inc("somekey")
+	}
+
+	time.Sleep(time.Second)
+	fmt.Println(c.Value("somekey"))
+}
